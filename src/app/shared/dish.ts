be445edc11d216f0957c98ef91dcfec49d2600de
junahmkh <junahmkh@hotmail.com>
2022-04-22 22:02:30 +0200

    angular component part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+    id: string = '';
+    name: string = '';
+    image: string = '';
+    category: string = '';
+    featured!: boolean;				//either this
+    label: string = '';				//or this
+    price: string = '';				// its because of strict type def of javascript
+    description: string = '';
+}
