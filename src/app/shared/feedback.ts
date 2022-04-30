256faa57738dc3a0ba88a32108fb45c2fc797640
junahmkh <junahmkh@hotmail.com>
2022-04-30 11:17:48 +0200

    reactive forms part 1

---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,11 @@
+export class Feedback {
+    firstName!: string;
+    lastName!: string;
+    telnum!: number;
+    email!: string;
+    agree!: boolean;
+    contacttype!: string;
+    message!: string;
+}
+
+export const ContactType = ['None', 'Tel','Email'];
